dff05cb383fb2a3c818cc8871a088d54b11107e2
Toru Ikeda <toru48@outlook.jp>
2019-09-29 12:48:55 +0900

    #3 add assign execution feature

--- src/Nabla/Executor.hs
+++ src/Nabla/Executor.hs
@@ -1,9 +1,36 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving  #-}
+
 module Nabla.Executor where
 
+import Control.Monad.Identity
+import Control.Monad.State
+import Control.Monad.Except
 import Nabla.AST
 
-exec :: AST -> IO (Either String ())
-exec (AST []) = pure $ Right ()
-exec (AST (expr:exprs)) = do
-  print expr
-  exec $ AST exprs
+newtype Executor a = Executor (StateT [Var] (ExceptT String Identity) a)
+  deriving (Functor, Applicative, Monad, MonadState [Var], MonadError String)
+
+runExecutor :: Executor a -> [Var] -> Either String (a, [Var])
+runExecutor (Executor exec) vars = runIdentity (runExceptT (runStateT exec vars))
+
+type Var = (Identifier, Value)
+
+exec :: AST -> Executor [Value]
+exec (AST []) = return []
+exec (AST (expr:exprs)) = (:) <$> (eval expr) <*> exec (AST exprs)
+
+eval :: Expr -> Executor Value
+eval (ValueExpr v) = return v
+eval (VariableExpr name) = do
+  vars <- get
+  case lookup name vars of
+    Nothing -> throwError $ name <> " is not assign yet."
+    Just v -> return v
+eval (Assign name v) = do
+  vars <- get
+  case lookup name vars of
+    Just _ -> throwError $ name <> " is already assign."
+    Nothing -> return v
+  v' <- eval v
+  put $ (name, v'):vars
+  return v'
