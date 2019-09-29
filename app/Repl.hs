1fa2a540934532bfc230809464ffd5b019f611ff
Toru Ikeda <toru48@outlook.jp>
2019-09-29 12:51:59 +0900

    #3 add state management for vars feature to repl

--- app/Repl.hs
+++ app/Repl.hs
@@ -1,29 +1,58 @@
+{-# LANGUAGE DeriveFunctor, GeneralizedNewtypeDeriving  #-}
+
 module Repl where
 
 import Control.Monad.IO.Class
 import Control.Monad (when)
+import Control.Monad.Identity
+import Control.Monad.State
+import Control.Monad.Except
+import Data.List (intercalate)
 import System.Exit (exitSuccess, exitFailure)
 import Text.Megaparsec (parse)
 import Nabla.Parser (ast)
-import Nabla.Executor (exec)
+import Nabla.Executor (exec, Var, runExecutor)
 import System.Console.Haskeline
 import Nabla.AST
 
 main :: IO ()
-main = runInputT defaultSettings repl
+main = repl []
 
-repl :: InputT IO ()
-repl = do
-  line <- getInputLine "nabla> "
-  case line of
-    Nothing -> liftIO exitFailure
-    Just l -> do
-      when (l == ".exit") $ liftIO $ putStrLn "ByeBye~(^q^)" >> exitSuccess
-      case astParse l of
-        Right ast -> liftIO $ exec ast
+newtype REPL a = REPL (StateT [Var] (ExceptT String IO) a)
+  deriving (Functor, Applicative, Monad, MonadState [Var], MonadError String)
+
+runREPL :: REPL a -> [Var] -> IO (Either String (a, [Var]))
+runREPL (REPL repl) vars = runExceptT (runStateT repl vars)
+
+repl :: [Var] -> IO ()
+repl vars = runInputT defaultSettings (repl' vars)
+  where
+    repl' vars = do
+      line' <- getInputLine "nabla> "
+      line <- case line' of
+        Nothing -> return ""
+        Just line -> return line
+      when (line == ".exit") $ liftIO $ putStrLn "ByeBye~(^q^)" >> exitSuccess
+      res <- liftIO $ runREPL (replOneExpr line) vars
+      newVars <- case res of
+        Right (values, vars') -> do
+          lift $ putStrLn $ intercalate "\n" $ map show values
+          return vars'
         Left e -> do
-          liftIO $ print e
-          pure $ Left $ show e
-      repl
+          lift $ putStrLn e
+          return vars
+      repl' newVars
+
+replOneExpr :: String -> REPL [Value]
+replOneExpr line = do
+  vars <- get
+  ast <- case astParse line of
+    Right ast -> return ast
+    Left e -> throwError $ show e
+  (vs, vars') <- case runExecutor (exec ast) vars of
+    Right res -> return res
+    Left e -> throwError e
+  put $ vars ++ vars'
+  return vs
 
 astParse line = parse ast "repl" (line <> "\n")
