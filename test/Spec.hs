68ccae7d871f632a752ea062ae8558857c202724
Toru Ikeda <toru48@outlook.jp>
2019-11-17 23:30:41 +0900

    exit failure if tests of error or failure exist

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,13 @@
+import System.Exit
 import Test.HUnit
 import System.IO
 import ParserTest
 
-main = runTestText (putTextToHandle stderr False) tests
+main = do
+  counts <- runTestTT tests
+  if (errors counts) + (failures counts) > 0
+    then exitWith $ ExitFailure 1
+    else exitSuccess
 
 tests = TestList $ concat
   [ simpleValueTest
