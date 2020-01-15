2d40e2f44b574939df528de97d268074e5b0b510
Toru Ikeda <toru48@outlook.jp>
2020-01-15 21:56:23 +0900

    fix test because of changing ast

--- test/ParserTest.hs
+++ test/ParserTest.hs
@@ -6,42 +6,44 @@ import Nabla.Parser
 import Nabla.AST
 
 simpleV t str = ValueExpr (SimpleV (t str))
+simpleV' t str = ExprUnit (ValueExpr (SimpleV (t str)))
 complexV str vs = ValueExpr (ComplexV (WrapValues str vs))
+complexV' str vs = ExprUnit (ValueExpr (ComplexV (WrapValues str vs)))
 
 simpleValueTest =
-  [ "number" ~: parse ast "" "1\n" ~?= Right (AST [(simpleV NumberV "1")])
-  , "number" ~: parse ast "" "+1\n" ~?= Right (AST [(simpleV NumberV "1")])
-  , "number" ~: parse ast "" "-1\n" ~?= Right (AST [(simpleV NumberV "-1")])
-  , "number" ~: parse ast "" "1.0\n" ~?= Right (AST [(simpleV NumberV "1.0")])
-  , "number" ~: parse ast "" "+1.0\n" ~?= Right (AST [(simpleV NumberV "1.0")])
-  , "number" ~: parse ast "" "-1.0\n" ~?= Right (AST [(simpleV NumberV "-1.0")])
-  , "string" ~: parse ast "" "'abc'\n" ~?= Right (AST [(simpleV StringV "abc")])
-  , "string" ~: parse ast "" "'a b c'\n" ~?= Right (AST [(simpleV StringV "a b c")])
-  , "string" ~: parse ast "" "'あいうえお'\n" ~?= Right (AST [(simpleV StringV "あいうえお")])
-  , "string" ~: parse ast "" "''\n" ~?= Right (AST [(simpleV StringV "")])
-  , "string" ~: parse ast "" "' '\n" ~?= Right (AST [(simpleV StringV " ")])
-  , "symbol" ~: parse ast "" ":a\n" ~?= Right (AST [(simpleV SymbolV "a")])
-  , "symbol" ~: parse ast "" ":A\n" ~?= Right (AST [(simpleV SymbolV "A")])
-  , "symbol" ~: parse ast "" ":abc\n" ~?= Right (AST [(simpleV SymbolV "abc")])
-  , "symbol" ~: parse ast "" ":ABC\n" ~?= Right (AST [(simpleV SymbolV "ABC")])
-  , "symbol" ~: parse ast "" ":1\n" ~?= Right (AST [(simpleV SymbolV "1")])
-  , "symbol" ~: parse ast "" ":123\n" ~?= Right (AST [(simpleV SymbolV "123")])
+  [ "number" ~: parse ast "" "1\n" ~?= Right (AST [(simpleV' NumberV "1")])
+  , "number" ~: parse ast "" "+1\n" ~?= Right (AST [(simpleV' NumberV "1")])
+  , "number" ~: parse ast "" "-1\n" ~?= Right (AST [(simpleV' NumberV "-1")])
+  , "number" ~: parse ast "" "1.0\n" ~?= Right (AST [(simpleV' NumberV "1.0")])
+  , "number" ~: parse ast "" "+1.0\n" ~?= Right (AST [(simpleV' NumberV "1.0")])
+  , "number" ~: parse ast "" "-1.0\n" ~?= Right (AST [(simpleV' NumberV "-1.0")])
+  , "string" ~: parse ast "" "'abc'\n" ~?= Right (AST [(simpleV' StringV "abc")])
+  , "string" ~: parse ast "" "'a b c'\n" ~?= Right (AST [(simpleV' StringV "a b c")])
+  , "string" ~: parse ast "" "'あいうえお'\n" ~?= Right (AST [(simpleV' StringV "あいうえお")])
+  , "string" ~: parse ast "" "''\n" ~?= Right (AST [(simpleV' StringV "")])
+  , "string" ~: parse ast "" "' '\n" ~?= Right (AST [(simpleV' StringV " ")])
+  , "symbol" ~: parse ast "" ":a\n" ~?= Right (AST [(simpleV' SymbolV "a")])
+  , "symbol" ~: parse ast "" ":A\n" ~?= Right (AST [(simpleV' SymbolV "A")])
+  , "symbol" ~: parse ast "" ":abc\n" ~?= Right (AST [(simpleV' SymbolV "abc")])
+  , "symbol" ~: parse ast "" ":ABC\n" ~?= Right (AST [(simpleV' SymbolV "ABC")])
+  , "symbol" ~: parse ast "" ":1\n" ~?= Right (AST [(simpleV' SymbolV "1")])
+  , "symbol" ~: parse ast "" ":123\n" ~?= Right (AST [(simpleV' SymbolV "123")])
   ]
 
 complexValueTest =
-  [ "complex" ~: parse ast "" "Nothing\n" ~?= Right (AST [(complexV "Nothing" [])])
+  [ "complex" ~: parse ast "" "Nothing\n" ~?= Right (AST [(complexV' "Nothing" [])])
   , "complex" ~: parse ast "" "Just 1\n" ~?=
-    Right (AST [ (complexV "Just" [simpleV NumberV "1"]) ])
+    Right (AST [ (complexV' "Just" [simpleV NumberV "1"]) ])
   , "complex" ~: parse ast "" "Just Just 1\n" ~?=
       Right (AST
-        [ (complexV "Just"
+        [ (complexV' "Just"
             [ complexV "Just" [ simpleV NumberV "1" ] ]
           )
         ]
       )
   , "complex" ~: parse ast "" "List 1 2\n" ~?=
       Right (AST
-        [ (complexV "List"
+        [ (complexV' "List"
             [ simpleV NumberV "1"
             , simpleV NumberV "2"
             ]
@@ -50,7 +52,7 @@ complexValueTest =
       )
   , "complex" ~: parse ast "" "List Just 1\n" ~?=
       Right (AST
-        [ (complexV "List"
+        [ (complexV' "List"
             [ complexV "Just" [ simpleV NumberV "1" ]
             ]
           )
@@ -58,7 +60,7 @@ complexValueTest =
       )
   , "complex" ~: parse ast "" "List Just 'a' Just 'b'\n" ~?=
       Right (AST
-        [ (complexV "List"
+        [ (complexV' "List"
             [ complexV "Just"
               [ simpleV StringV "a"
               , complexV "Just"
@@ -71,7 +73,7 @@ complexValueTest =
       )
   , "complex" ~: parse ast "" "List (Just 'a') (Just 'b')\n" ~?=
     Right (AST
-      [ (complexV "List"
+      [ (complexV' "List"
           [ complexV "Just" [ simpleV StringV "a" ]
           , complexV "Just" [ simpleV StringV "b" ]
           ]
@@ -82,19 +84,19 @@ complexValueTest =
 
 variableExprTest =
   [ "assign" ~: parse ast "" "a\n" ~?=
-    Right (AST [(VariableExpr "a")])
+    Right (AST [ExprUnit (VariableExpr "a")])
   , "assign" ~: parse ast "" "a1\n" ~?=
-    Right (AST [(VariableExpr "a1")])
+    Right (AST [ExprUnit (VariableExpr "a1")])
   , "assign" ~: parse ast "" "aA\n" ~?=
-    Right (AST [(VariableExpr "aA")])
+    Right (AST [ExprUnit (VariableExpr "aA")])
   , "assign" ~: parse ast "" "a1A\n" ~?=
-    Right (AST [(VariableExpr "a1A")])
+    Right (AST [ExprUnit (VariableExpr "a1A")])
   ]
 
 assignExprTest =
   [ "assign" ~: parse ast "" "a = 1\n" ~?= Right
     (AST [
-      (Assign "a"
+      ExprUnit (Assign "a"
         (ValueExpr
           (SimpleV (NumberV "1"))
         )
@@ -102,7 +104,7 @@ assignExprTest =
     ])
   , "assign" ~: parse ast "" "a = Just 1\n" ~?= Right
     (AST [
-      (Assign "a"
+      ExprUnit (Assign "a"
         (ValueExpr
           (ComplexV
             (WrapValues "Just" [ValueExpr (SimpleV (NumberV "1"))])
@@ -112,7 +114,7 @@ assignExprTest =
     ])
   , "assign" ~: parse ast "" "a = b\n" ~?= Right
     (AST [
-      (Assign "a"
+      ExprUnit (Assign "a"
         (VariableExpr "b")
       )
     ])
