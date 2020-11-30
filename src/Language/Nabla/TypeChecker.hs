8fcd1f52006545fb2c1f8aa18d5dcba7c831dc9b
Toru Ikeda <toru48@outlook.jp>
2020-11-30 23:38:43 +0900

    add ProgDict ProgDict (Program Dictionary) is functions collection for type checking

--- src/Language/Nabla/TypeChecker.hs
+++ src/Language/Nabla/TypeChecker.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Language.Nabla.TypeChecker where
 
@@ -8,23 +9,76 @@ import Language.Nabla.AST
 import Language.Nabla.Fixture
 import Data.SBV
 import Control.Concurrent.Async
+import Control.Lens (makeLenses, (^.), (.~), (?~), (&))
+import Control.Lens.Operators ((<|))
 
-check :: Prog p -> [TypeValidationError p]
-check (Prog us) = valid (Prog us) (us <> map (\(name, t) -> NamedUnit (name, UnitFnType t)) fixtureFnTypes)
+data ProgDict p
+  = ProgDict
+  { _types :: [(Identifier p, TypeDef p)]
+  , _fns :: [(Identifier p, FnDef p)]
+  }
+data FnDef p
+  = FnDef
+  { _fn :: Maybe (Fn p)
+  , _fnType :: Maybe (FnType p)
+  , _fnInferredType :: Maybe (FnType p)
+  }
+
+makeLenses ''ProgDict
+makeLenses ''FnDef
+
+newProgDict :: ProgDict p
+newProgDict = ProgDict [] []
+
+addFn :: Identifier p -> Fn p -> ProgDict p -> ProgDict p
+addFn name f dict = case lookup name (dict ^. fns) of
+  Nothing -> dict & fns .~ (name, FnDef (Just f) Nothing Nothing):(dict ^. fns)
+  Just fnDef -> dict & fns .~ ((name, fnDef & fn ?~ f):delete name (dict ^. fns))
+
+addFnType :: Identifier p -> FnType p -> ProgDict p -> ProgDict p
+addFnType name sig dict = case lookup name (dict ^. fns) of
+  Nothing -> dict & fns .~ (name, FnDef Nothing (Just sig) Nothing):(dict ^. fns)
+  Just fnDef -> dict & fns .~ ((name, fnDef & fnType ?~ sig):delete name (dict ^. fns))
+
+addTypeDef :: Identifier p -> TypeDef p -> ProgDict p -> ProgDict p
+addTypeDef name tp dict = case lookup name (dict ^. types) of
+  Nothing -> dict & types .~ (name, tp):(dict ^. types)
+  Just _ -> undefined -- duplicate error
+
+delete :: Eq k => k -> [(k, a)] -> [(k, a)]
+delete k = filter (not . (==) k . fst)
+
+mkProgDict :: Prog p -> ProgDict p -> ProgDict p
+mkProgDict (Prog []) dict = dict
+mkProgDict (Prog ((NamedUnit (name, UnitFn fn)):us)) dict
+  = mkProgDict (Prog us) $ addFn name fn dict
+mkProgDict (Prog ((NamedUnit (name, UnitFnType fnType)):us)) dict
+  = mkProgDict (Prog us) $ addFnType name fnType dict
+mkProgDict (Prog ((NamedUnit (name, UnitTypeDef tp)):us)) dict
+  = mkProgDict (Prog us) $ addTypeDef name tp dict
 
 class TypeInference a where
   infer :: a -> [Type p]
 
 class TypeCheck a where
-  valid :: a p -> [NamedUnit p] -> [TypeValidationError p]
+  valid :: a p -> ProgDict p -> [TypeValidationError p]
 
-data TypeValidationError p = NameNotFound (Identifier p)
+data TypeValidationError p
+  = NameNotFound (Identifier p)
+  | ImplementationNotFound (Identifier p)
+
+check :: Prog p -> [TypeValidationError p]
+check (Prog us) = valid (Prog us) $
+  mkProgDict
+  (Prog (us <> map (\(name, t) -> NamedUnit (name, UnitFnType t)) fixtureFnTypes))
+  newProgDict
 
 getPos :: TypeValidationError p -> p
 getPos (NameNotFound (Identifier p _)) = p
 
 instance Show (TypeValidationError p) where
   show (NameNotFound name) = "name '" <> show name <> "' is not defined"
+  show (ImplementationNotFound name) = "the type signature for '" <> show name <> "' is defined, but an accompanying function is not implemented"
 
 instance TypeCheck Prog where
   valid (Prog (u:us)) ts = valid u ts <> valid (Prog us) ts
@@ -36,26 +90,29 @@ instance TypeCheck NamedUnit where
   valid (NamedUnit (name, UnitTypeDef e)) us = []
 
 instance TypeCheck Fn where
-  valid (Fn _ body) us = valid body us
+  valid (Fn args body) dict = valid body (foldl (\d (name, f) -> addFn name f d) dict (map toFn args))
+    where
+      toFn name = (name, Fn [] undefined)
 
 instance TypeCheck Expr where
   valid (Expr _ v []) us = valid v us
   valid (Expr p v vs) us = valid v us <> valid (Expr p (head vs) (tail vs)) us
 
 instance TypeCheck Value where
-  valid (Alias name) us = case findByName name us of
+  valid (Alias name) dict = case findByName name (dict ^. fns) of
     Right _ -> []
     Left e -> [e]
   valid (FnValue fn) us = valid fn us
   valid (Const _ _ ) _ = []
   valid (ExprValue expr) us = valid expr us
 
-findByName :: Identifier p -> [NamedUnit p] -> Either (TypeValidationError p) (Unit p)
-findByName name nus = case lookup name us of
+findByName :: Identifier p -> [(Identifier p, a)] -> Either (TypeValidationError p) a
+findByName name us = case lookup name us of
   Just a -> Right a
   Nothing -> Left $ NameNotFound name
-  where
-    us = map (\(NamedUnit u) -> u) nus
+
+-- isTypeOf :: Expr p -> TypeDef p -> [NamedUnit p] -> Bool
+-- isTypeOf e t us = (elementType t) e
 
 -- instance TypeCheck (Expr p) where
 --   valid (Expr p v vs) types =
@@ -65,6 +122,8 @@ findByName name nus = case lookup name us of
 
 --f :: Integer -> Integer -> Integer
 f a b = (a / b) + (a `div` b)
+
+-- a = Expr $ Alias []
 
 
 -- checkFnDif :: [(Identifier, FnDef)] -> FnDef -> IO (Either (String, m) ())
